b7ed640b01fd72f5950955cc0abd36ad3366329e
FrackenToaster <frakentoaster@gmail.com>
2015-07-12 19:11:48 -0700

    complete 02_calculator

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -8,10 +8,31 @@ end
 
 def sum(arr)
 	if arr.empty?
-		return 0
+		0
 	else
-		return arr.inject(:+)
+		arr.inject(:+)
 	end
 end
 
+def multiply(*num)
+	result = 1
+	num.each { |x| result *= x }
+	result
+end
 
+def power(x,y)
+	x**y
+end
+
+def factorial(num)
+	if num <= 1 
+		return 1
+	end
+
+	result = 1
+	while num > 0
+		result *= num
+		num -= 1
+	end
+	return result
+end
